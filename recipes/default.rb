a8a75d8b193f46ea3629a315ae58c9cd70ce420d
Sebastian Boehm <sebastian@sometimesfood.org>
2013-04-20 15:35:22 +0200

    Make libpam-krb5 installation optional

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,21 +8,10 @@ template "/etc/krb5.conf" do
   only_if { !node[:kerberos][:default_realm].empty? }
 end
 
-package "libpam-krb5" do
-  response_file "libpam-krb5.seed"
-end
-
-# Since this is a potential security risk do not install by default
-# and ensure it is purged if not wanted
-if node["kerberos"]["pam"]
-    execute "activate krb pam" do
-      command "pam-auth-update --package"
-    end
-else
-    execute "deactivate krb in pam" do
-      command "pam-auth-update --remove krb5"
-    end
+if node ["kerberos"]["pam"]
+  package "libpam-krb5" do
+    response_file "libpam-krb5.seed"
+  end
 end
 
 k5login node[:kerberos][:machine_admins]
-
